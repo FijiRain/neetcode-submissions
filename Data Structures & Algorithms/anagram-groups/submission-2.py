81ee6d47b6be70b30c53437e47fe37785385e5fb
FijiRain <104636244+FijiRain@users.noreply.github.com>
2026-07-19 10:31:30 +0200

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, str1: str, str2: str) -> bool:
+        if len(str1) != len(str2):
+            return False
+        return sorted(str1) == sorted(str2)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        exclude = []
+
+        for i in strs:
+            if i in exclude:
+                continue
+            sublist = []
+            for j in strs:
+                if self.isAnagram(i, j):
+                    sublist.append(j)
+                    exclude.append(j)
+            res.append(list(sublist))
+
+
+        return res
